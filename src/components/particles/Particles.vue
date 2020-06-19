c5ec47e33c61ef386be8901cb4c2220827577d6b
He939629374 <32150775+He939629374@users.noreply.github.com>
2020-06-20 01:16:31 +0800

    20200620
    20200320

---
+++ src/components/particles/Particles.vue
@@ -0,0 +1,28 @@
+<style scoped>
+  @import "./style.css";
+</style>
+<template>
+  <div id="contents">
+    <div id="particles-js"></div>
+  </div>
+</template>
+<style  scoped>
+</style>
+<script>
+  import "particles.js";
+  import particlesConfig from "./particles.json";
+  export default {
+    data() {
+      return {};
+    },
+    mounted() {
+      this.init();
+    },
+    methods: {
+        init(config = particlesConfig) {
+            // eslint-disable-next-line no-undef
+            particlesJS('particles-js', config);
+        },
+    }
+  };
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,62 @@
+<template>
+<div id="particlesId"> 
+  <particlesJS ></particlesJS> 
+</div>
+
+</template>
+<style scoped>
+
+
+</style>
+<script>
+import particlesJS from '../components/particles/Particles.vue'
+  export default {
+    name: 'login',
+    data() {
+      return {
+        vedioCanPlay: false,
+        fixStyle: '',
+        imgUrl: require('@/assets/soogif.gif')
+      }
+    },
+    components:{
+      particlesJS
+    },
+    methods: {
+      canplay() {
+        this.vedioCanPlay = true
+      },
+
+    },
+    mounted: function() {
+
+      window.onresize = () => {
+        const windowWidth = document.body.clientWidth
+        const windowHeight = document.body.clientHeight
+        const windowAspectRatio = windowHeight / windowWidth
+        let videoWidth
+        let videoHeight
+        if (windowAspectRatio < 0.5625) {
+          videoWidth = windowWidth
+          videoHeight = videoWidth * 0.5625
+          this.fixStyle = {
+            height: windowWidth * 0.5625 + 'px',
+            width: windowWidth + 'px',
+            'margin-bottom': (windowHeight - videoHeight) / 2 + 'px',
+            'margin-left': 'initial'
+          }
+        } else {
+          videoHeight = windowHeight
+          videoWidth = videoHeight / 0.5625
+          this.fixStyle = {
+            height: windowHeight + 'px',
+            width: windowHeight / 0.5625 + 'px',
+            'margin-left': (windowWidth - videoWidth) / 2 + 'px',
+            'margin-bottom': 'initial'
+          }
+        }
+      }
+      window.onresize()
+    }
+  }
+</script>
